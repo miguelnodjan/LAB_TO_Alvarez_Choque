127f75f85f5f899df0359ee1908bfc217292dadc
miguelnodjan <malvarezcho@unsa.edu.pe>
2025-09-18 08:38:21 -0500

    avance 1

---
+++ cambio.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int countChange(int valorCambio, vector<int> den){
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    vector <int> denominaciones;
+    int cambio;
+    int nueDen;
+    cout<< "Ingresa la cantidad de cambio a dar: ";
+    cin >> cambio;
+    cout << "Ingresar las denominaciones de moneda 0 cuando termine";
+    while (true)
+    {
+        cin >> nueDen;
+        cout << endl;
+        if (nueDen = 0)
+        {
+            break;
+        }
+        denominaciones.at(nueDen);
+    }
+    
+    return 0;
+}
