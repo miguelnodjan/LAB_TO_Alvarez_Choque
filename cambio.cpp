5d92c1b7cd325dc922ea858cf3be9ae9723f6402
miguelnodjan <malvarezcho@unsa.edu.pe>
2025-09-21 22:22:55 -0500

    completando la logica de la funcion countChange

--- cambio.cpp
+++ cambio.cpp
@@ -2,28 +2,37 @@
 #include <vector>
 using namespace std;
 
-int countChange(int valorCambio, vector<int> den){
-    
+int countChange(int money, vector<int> coins) {
+    if (money == 0) {
+        return 1;
+    }
+    if (money < 0 || coins.empty()) {
+        return 0;
+    }
+    return countChange(money - coins[0], coins) + countChange(money, vector<int>(coins.begin() + 1, coins.end()));
 }
 
 int main(int argc, char const *argv[])
 {
-    vector <int> denominaciones;
+    vector<int> denominaciones;
     int cambio;
     int nueDen;
-    cout<< "Ingresa la cantidad de cambio a dar: ";
+    
+    cout << "Ingresa la cantidad de cambio a dar: ";
     cin >> cambio;
-    cout << "Ingresar las denominaciones de moneda 0 cuando termine";
+    
+    cout << "Ingresar las denominaciones de moneda (0 cuando termine): ";
     while (true)
     {
         cin >> nueDen;
-        cout << endl;
-        if (nueDen = 0)
-        {
+        if (nueDen == 0) {
             break;
         }
-        denominaciones.at(nueDen);
+        denominaciones.push_back(nueDen);
     }
+    
+    int resultado = countChange(cambio, denominaciones);
+    cout << "El número de formas diferentes de dar cambio es: " << resultado << endl;
     
     return 0;
 }
